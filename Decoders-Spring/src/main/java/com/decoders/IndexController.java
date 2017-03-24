0c495fc05b77707b8cf0d3697fd42a081dfe0108
raghav <rarora9@sapient.com>
2017-03-24 13:55:16 +0530

    Initial commit

---
+++ Decoders-Spring/src/main/java/com/decoders/IndexController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.decoders;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author raror3
+ *
+ */
+@Controller
+public class IndexController {
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
+	@RequestMapping("/")
+    public String welcomeDecoders(@RequestParam(value="name", required=false, defaultValue="decoders") String name, Model model) {
+        model.addAttribute("name", name);
+        log.info("Hello Name as: " + name);
+        return "welcomeDecoders";
+    }
+}
